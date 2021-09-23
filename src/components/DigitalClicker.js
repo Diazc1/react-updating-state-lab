b8e90049bcd8c9f467d774be6a1b0f078e1705d6
Cathy Diaz <Cathydiaz14@gmail.com>
2021-09-23 11:39:53 -0700

    commit tree ec96158ca0fc1421cc24cb5ec37ae6b10bf857b5

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
 // Code DigitalClicker Component Here
+
+import React, { Component } from "react";
+
+export default class DigitalClicker extends Component {
+
+    constructor() {
+        super()
+            //initial state
+            //This component has an initial state property called timesClicked, which is initially defined as 0.
+            this.state = {
+                timesClicked: 0,
+            };
+    }
+
+    handleClick = () => {
+        this.setState(prevState => ({
+            timesClicked: prevState.timesClicked+1
+        }))
+    }
+    
+
+    render() {
+        return (
+        <div>
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+        </div>
+        )
+    }
+}
